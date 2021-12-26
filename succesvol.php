cfc7736e706dc94b93fa9c5d614a62d87c10512f
jemaak <64607309+yassinejwzlol@users.noreply.github.com>
2021-12-26 22:27:33 +0100

    Add succesvol.php

---
+++ succesvol.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="assets/css/style.css" type="text/css">
+    </head>
+    <body>
+        <div class="text">
+            <h1>
+                Profile is succesfully added to our database!!
+            </h1>
+            <br>
+            <button class="success-button" onclick="window.location.href='putpeople.php';">
+                Put people in a house
+            </button>
+        </div>
+    </body>
+</html>
