c468402f1396901250b8eb21c6b5512abf390ff9
jemaak <64607309+yassinejwzlol@users.noreply.github.com>
2021-12-26 22:26:21 +0100

    Add index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="assets/css/style.css" type="text/css">
+    </head>
+    <body>
+        <img id="index-foto" src="assets/pictures/harry potter en zijn vrienden.jpg" alt="harry potter en zijn vrienden">
+        <br><br>
+        <div id="buttons">
+            <button class="index-button" onclick="window.location.href='invulformulier.php';">
+                Add person
+            </button>
+            <button class="index-button" onclick="window.location.href='putpeople.php';">
+                Put people in a house
+            </button>
+        </div>
+    </body>
+</html>
